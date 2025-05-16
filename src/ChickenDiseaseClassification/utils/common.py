10a4375b7fc0e36ba630b667f0a9aaeaf7aebd3f
umraliya-kevin <kevinumraliya@gmail.com>
2025-05-16 12:00:58 +0530

    data ingestion completed

--- src/ChickenDiseaseClassification/utils/common.py
+++ src/ChickenDiseaseClassification/utils/common.py
@@ -55,3 +55,14 @@ def create_directories(path_to_directories: list, verbose=False):
         os.makedirs(path, exist_ok=True)
         if verbose:
             logging.info(f"created directory at: {path}")
+            
+@ensure_annotations
+def get_size(path: Path) -> str:
+    """get size in KB
+    Args:
+        path (Path): path of the file
+    Returns:
+        str: size in KB
+    """
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~ {size_in_kb} KB"
